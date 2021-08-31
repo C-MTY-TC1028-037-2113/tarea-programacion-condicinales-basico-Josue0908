703e1618d710915b04baef0a0ffb7ecadfa33901
Josue Morales <A00832701@itesm.mx>
2021-08-30 19:24:24 -0500

    entrega de la tarea

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,7 +1,17 @@
 
 def main():
     edad = int(input("Ingresa tu edad: "))
-    #Aquí empieza tu programa...
+    identificacion = ""
+    
+    if edad >= 18:
+        identificacion = input("¿Tienes identificación oficial? (s/n): ")
+        if identificacion == "s" or identificacion == "S":
+            print("Trámite de licencia concedido")
+        elif edad < 18 or identificacion == "n" or identificacion == "N":
+            print("No cumples requisitos")
+        else: 
+            print("Respuesta incorrecta")
+
 
 if __name__ == '__main__':
     main()
--- assignments/06BMI/src/exercise.py
+++ assignments/06BMI/src/exercise.py
@@ -1,6 +1,24 @@
 def main():
     #escribe tu código abajo de esta línea
     
+    peso = float(input("Peso en kg: "))
+    altura = float(input("Altura en m: "))
+
+    if altura <= 0 or peso <= 0:
+        print("Revisa tus datos, alguno de ellos es erróneo.")
+    elif altura > 0:
+        indice = peso / altura**2
+        if indice < 20:
+            print("PESO BAJO")
+        elif 20 <= indice < 25:
+            print("NORMAL")
+        elif 25 <= indice < 30:
+            print("SOBREPESO")
+        elif 30 <= indice < 40:
+            print("OBESIDAD")
+        elif indice >= 40:
+            print("OBESIDAD MORBIDA")
+    
 
 if __name__=='__main__':
     main()
